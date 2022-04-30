969bbe5034711aedeecb6e299bc7307b34940282
戈小荷 <gehongyan1996@126.com>
2022-04-30 20:46:26 +0800

    Added a sample to realize a reaction role bot.

---
+++ samples/KaiHeiLa.Net.Samples.ReactionRoleBot/Configurations/KaiHeiLaBotConfigurations.cs
@@ -0,0 +1,36 @@
+﻿using KaiHeiLa.Net.Samples.ReactionRoleBot.Enums;
+
+namespace KaiHeiLa.Net.Samples.ReactionRoleBot.Configurations;
+
+public class KaiHeiLaBotConfigurations
+{
+    /// <summary>
+    ///     Token
+    /// </summary>
+    public string Token { get; init; }
+
+    /// <summary>
+    ///     频道ID列表
+    /// </summary>
+    public KaiHeiLaChannels Channels { get; init; }
+
+    /// <summary>
+    ///     角色ID列表
+    /// </summary>
+    public KaiHeiLaRoles Roles { get; init; }
+    
+    /// <summary>
+    ///     开黑啦Bot运行环境
+    /// </summary>
+    public BotEnvironment KaiHeiLaBotEnvironment { get; init; }
+}
+
+public class KaiHeiLaChannels
+{
+    public ulong ReactionChannelId { get; init; }
+}
+
+public class KaiHeiLaRoles
+{
+    public ulong DeveloperRoleId { get; init; }
+}
---
+++ samples/KaiHeiLa.Net.Samples.ReactionRoleBot/Enums/BotEnvironment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace KaiHeiLa.Net.Samples.ReactionRoleBot.Enums;
+
+public enum BotEnvironment
+{
+    /// <summary>
+    ///     调试
+    /// </summary>
+    [Description("调试")]
+    Development,
+
+    /// <summary>
+    ///     生产
+    /// </summary>
+    [Description("生产")]
+    Production,
+}
