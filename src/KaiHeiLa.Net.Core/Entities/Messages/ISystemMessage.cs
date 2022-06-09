363ed46325c9027d778a567d3d7cf18c8f27c429
戈小荷 <gehongyan1996@126.com>
2022-06-09 19:28:07 +0800

    Added SystemMessageType warning doc

--- src/KaiHeiLa.Net.Core/Entities/Messages/ISystemMessage.cs
+++ src/KaiHeiLa.Net.Core/Entities/Messages/ISystemMessage.cs
@@ -8,5 +8,12 @@ public interface ISystemMessage : IMessage
     /// <summary>
     ///     Gets the type of the system message.
     /// </summary>
+    /// <remarks>
+    ///     <note type="warning">
+    ///         Because the data from KaiHeiLa does not describe the type
+    ///         of the message in detail, this property is not implemented yet.
+    ///         Accessing this property will always result in an exception at present.
+    ///     </note>
+    /// </remarks>
     SystemMessageType SystemMessageType { get; }
 }
