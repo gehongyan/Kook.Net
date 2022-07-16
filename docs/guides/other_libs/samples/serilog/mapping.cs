c660a74e83ec4fb74a07d7ab1e3f09401b33bb17
戈小荷 <gehongyan1996@126.com>
2022-07-16 14:00:04 +0800

    fix(doc): fix Serilog log level mapping

--- docs/guides/other_libs/samples/serilog/mapping.cs
+++ docs/guides/other_libs/samples/serilog/mapping.cs
@@ -6,8 +6,9 @@ private static async Task LogAsync(LogMessage message)
         LogSeverity.Error => LogEventLevel.Error,
         LogSeverity.Warning => LogEventLevel.Warning,
         LogSeverity.Info => LogEventLevel.Information,
-        LogSeverity.Verbose => LogEventLevel.Verbose,
-        LogSeverity.Debug => LogEventLevel.Debug,
+        // Serilog 中，LogEventLevel.Verbose 相比 LogEventLevel.Debug 会输出更多的信息
+        LogSeverity.Verbose => LogEventLevel.Debug,
+        LogSeverity.Debug => LogEventLevel.Verbose,
         _ => LogEventLevel.Information
     };
     Log.Write(severity, message.Exception, "[{Source}] {Message}", message.Source, message.Message);
