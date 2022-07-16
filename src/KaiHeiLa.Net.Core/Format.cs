bfae8646972ee98e9691cb7007e0bee40a1c342b
戈小荷 <gehongyan1996@126.com>
2022-07-16 13:41:36 +0800

    fix: Possible NRE in Sanitize

--- src/KaiHeiLa.Net.Core/Format.cs
+++ src/KaiHeiLa.Net.Core/Format.cs
@@ -35,6 +35,7 @@ public static class Format
     /// <summary> Sanitizes the string, safely escaping any Markdown sequences. </summary>
     public static string Sanitize(string text)
     {
+        if (text is null) return null;
         return SensitiveCharacters.Aggregate(text,
             (current, unsafeChar) => current.Replace(unsafeChar, $"\\{unsafeChar}"));
     }
