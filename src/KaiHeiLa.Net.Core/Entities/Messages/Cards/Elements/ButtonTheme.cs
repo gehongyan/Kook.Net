0d735c219a7e85c26d6b19e2903046316c5bc143
戈小荷 <gehongyan1996@126.com>
2022-07-21 23:29:42 +0800

    fix: add missing ButtonTheme items

--- src/KaiHeiLa.Net.Core/Entities/Messages/Cards/Elements/ButtonTheme.cs
+++ src/KaiHeiLa.Net.Core/Entities/Messages/Cards/Elements/ButtonTheme.cs
@@ -10,15 +10,23 @@ public enum ButtonTheme
     /// </summary>
     Primary,
     /// <summary>
+    ///     The button is a success button.
+    /// </summary>
+    Success,
+    /// <summary>
     ///     The button is a warning button.
     /// </summary>
     Warning,
+    /// <summary>
+    ///     The button is a danger button.
+    /// </summary>
+    Danger,
     /// <summary>
     ///     The button is an info button.
     /// </summary>
     Info,
     /// <summary>
-    ///     The button is an error button.
+    ///     The button is a secondary button.
     /// </summary>
-    Danger
+    Secondary,
 }
