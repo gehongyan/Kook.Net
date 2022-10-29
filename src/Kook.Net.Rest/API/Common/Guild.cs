ae31784436dde84416c89676566fea650316bd06
戈小荷 <gehongyan1996@126.com>
2022-10-29 17:57:04 +0800

    Change owner related property parsing from master_id to user_id

--- src/Kook.Net.Rest/API/Common/Guild.cs
+++ src/Kook.Net.Rest/API/Common/Guild.cs
@@ -8,20 +8,25 @@ internal class Guild
     [JsonPropertyName("id")]
     public ulong Id { get; set; }
 
-    [JsonPropertyName("name")] public string Name { get; set; }
-    [JsonPropertyName("topic")] public string Topic { get; set; }
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
 
-    [JsonPropertyName("master_id")]
+    [JsonPropertyName("topic")]
+    public string Topic { get; set; }
+
+    [JsonPropertyName("user_id")]
     public ulong OwnerId { get; set; }
 
-    [JsonPropertyName("icon")] public string Icon { get; set; }
+    [JsonPropertyName("icon")]
+    public string Icon { get; set; }
 
     [JsonPropertyName("notify_type")]
     public NotifyType NotifyType { get; set; }
 
-    [JsonPropertyName("region")] public string Region { get; set; }
+    [JsonPropertyName("region")]
+    public string Region { get; set; }
 
-    [JsonPropertyName("enable_open")] 
+    [JsonPropertyName("enable_open")]
     [JsonConverter(typeof(NumberBooleanConverter))]
     public bool EnableOpen { get; set; }
 
@@ -34,6 +39,9 @@ internal class Guild
     [JsonPropertyName("welcome_channel_id")]
     public ulong WelcomeChannelId { get; set; }
 
-    [JsonPropertyName("roles")] public Role[] Roles { get; set; }
-    [JsonPropertyName("channels")] public Channel[] Channels { get; set; }
+    [JsonPropertyName("roles")]
+    public Role[] Roles { get; set; }
+
+    [JsonPropertyName("channels")]
+    public Channel[] Channels { get; set; }
 }
