88f475ec1353473c19e84aea0b1c4aa501b8c9df
Gehongyan <gehongyan1996@126.com>
2023-06-03 15:55:27 +0800

    Add missing file

---
+++ src/Kook.Net.Rest/Net/Converters/DirectMessageQuoteFixtureConverter.cs
@@ -0,0 +1,20 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Kook.Net.Converters;
+
+internal class DirectMessageQuoteFixtureConverter : JsonConverter<API.Quote>
+{
+    /// <inheritdoc />
+    public override API.Quote Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        reader.TokenType switch
+        {
+            JsonTokenType.Null or JsonTokenType.String => null,
+            JsonTokenType.StartObject => JsonSerializer.Deserialize<API.Quote>(ref reader, options),
+            _ => throw new JsonException(
+                $"{nameof(NumberBooleanConverter)} expects boolean, string or number token, but got {reader.TokenType}")
+        };
+
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, API.Quote value, JsonSerializerOptions options) => throw new NotImplementedException();
+}
