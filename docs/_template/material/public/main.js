23ce6eeb4349437ae0d6f6c67728bdb491546846
Gehongyan <gehongyan1996@126.com>
2024-02-28 18:04:55 +0800

    Fix icon link

--- docs/_template/material/public/main.js
+++ docs/_template/material/public/main.js
@@ -4,11 +4,6 @@
             icon: 'github',
             href: 'https://github.com/gehongyan/Kook.Net',
             title: 'GitHub'
-        },
-        {
-            icon: 'discord',
-            href: 'https://discord.gg/dnet',
-            title: 'Discord'
         }
     ]
 }
