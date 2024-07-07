20e70bec634e1126ed6a75f45c8aa47f4b90c394
Gehongyan <gehongyan1996@126.com>
2024-07-07 22:42:00 +0800

    Changes in-memory queue implementation to BlockingCollection

--- src/Kook.Net.MessageQueue.InMemory/InMemoryMessageQueue.cs
+++ src/Kook.Net.MessageQueue.InMemory/InMemoryMessageQueue.cs
@@ -1,98 +1,43 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Concurrent;
+using System.Text.Json;
 
 namespace Kook.Net.Queue.InMemory;
 
 internal class InMemoryMessageQueue : BaseMessageQueue
 {
-#if NET6_0_OR_GREATER
-    private readonly PriorityQueue<JsonElement, int> _queue;
-#else
-    private readonly Queue<JsonElement> _queue;
-#endif
-    private readonly SemaphoreSlim _semaphore;
-    private CancellationTokenSource? _inMemoryQueueCancellationTokenSource;
-    private CancellationTokenSource? _dequeueWaitingCancellationTokenSource;
+    private readonly BlockingCollection<JsonElement> _queue;
+    private CancellationTokenSource? _cancellationTokenSource;
 
     internal InMemoryMessageQueue(Func<JsonElement, Task> eventHandler)
         : base(eventHandler)
     {
-        _semaphore = new SemaphoreSlim(1, 1);
-#if NET6_0_OR_GREATER
-        _queue = new PriorityQueue<JsonElement, int>();
-#else
-        _queue = new Queue<JsonElement>();
-#endif
+        _queue = [];
     }
 
     /// <inheritdoc />
     public override Task StartAsync(CancellationToken cancellationToken = default)
     {
-        _inMemoryQueueCancellationTokenSource?.Dispose();
-        _inMemoryQueueCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-        Task.Run(async () =>
+        _cancellationTokenSource?.Dispose();
+        _cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        Task.Factory.StartNew(async () =>
         {
-            while (!_inMemoryQueueCancellationTokenSource.Token.IsCancellationRequested)
-            {
-                JsonElement gatewayEvent = await DequeueAsync(_inMemoryQueueCancellationTokenSource.Token)
-                    .ConfigureAwait(false);
+            foreach (JsonElement gatewayEvent in _queue.GetConsumingEnumerable(_cancellationTokenSource.Token))
                 await EventHandler(gatewayEvent).ConfigureAwait(false);
-            }
-        }, _inMemoryQueueCancellationTokenSource.Token);
+        }, _cancellationTokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
         return Task.CompletedTask;
     }
 
     /// <inheritdoc />
     public override Task StopAsync(CancellationToken cancellationToken = default)
     {
-        _inMemoryQueueCancellationTokenSource?.Cancel();
-        _inMemoryQueueCancellationTokenSource?.Dispose();
-        _inMemoryQueueCancellationTokenSource = null;
+        _queue.CompleteAdding();
         return Task.CompletedTask;
     }
 
-    public override async Task EnqueueAsync(JsonElement payload, int sequence,
+    public override Task EnqueueAsync(JsonElement payload, int sequence,
         CancellationToken cancellationToken = default)
     {
-        await _semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
-        try
-        {
-#if NET6_0_OR_GREATER
-            _queue.Enqueue(payload, sequence);
-#else
-            _queue.Enqueue(payload);
-#endif
-            _dequeueWaitingCancellationTokenSource?.Cancel();
-            _dequeueWaitingCancellationTokenSource?.Dispose();
-            _dequeueWaitingCancellationTokenSource = null;
-        }
-        finally
-        {
-            _semaphore.Release();
-        }
-    }
-
-    private async Task<JsonElement> DequeueAsync(CancellationToken cancellationToken = default)
-    {
-        await _semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
-        try
-        {
-            if (_queue.Count > 0)
-                return _queue.Dequeue();
-            _dequeueWaitingCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-        }
-        finally
-        {
-            _semaphore.Release();
-        }
-
-        try
-        {
-            await Task.Delay(Timeout.InfiniteTimeSpan, _dequeueWaitingCancellationTokenSource.Token).ConfigureAwait(false);
-        }
-        catch (OperationCanceledException)
-        {
-            // ignored
-        }
-        return _queue.Dequeue();
+        _queue.Add(payload, cancellationToken);
+        return Task.CompletedTask;
     }
 }
