ba910411bc03a8a989110f2cbb8ff853f67ad7af
Gehongyan <gehongyan1996@126.com>
2024-10-27 20:37:49 +0800

    Fix Permissions.MaxBites

--- src/Kook.Net.Core/Utils/Permissions.cs
+++ src/Kook.Net.Core/Utils/Permissions.cs
@@ -4,7 +4,7 @@ namespace Kook;
 
 internal static class Permissions
 {
-    public const int MaxBits = 29;
+    public const int MaxBits = 30;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static PermValue GetValue(ulong allow, ulong deny, ChannelPermission flag) =>
