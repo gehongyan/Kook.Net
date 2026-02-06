1d2df775dad1a2da6521e9e0b919626a6adc3ed4
Gehongyan <gehongyan1996@126.com>
2026-02-06 23:10:50 +0800

    Better doc

--- src/Kook.Net.Core/Entities/Emotes/InteractiveEmote.cs
+++ src/Kook.Net.Core/Entities/Emotes/InteractiveEmote.cs
@@ -24,10 +24,11 @@ public class InteractiveEmote : Emote
     public InteractiveEmoteType InteractiveEmoteType { get; }
 
     /// <summary>
-    ///     获取此互动表情符号在显示随机结果前的动画资源图像的 URL。
+    ///     获取此互动表情符号在显示随机结果前显示的动画资源名称。
     /// </summary>
     /// <remarks>
-    ///     如果此动态表情符号不是从 KOOK API 或网关生成的，则此属性为 <c>null</c>。
+    ///     如果此动态表情符号不是从 KOOK API 或网关生成的，则此属性为 <c>null</c>。<br />
+    ///     要获取完整的资源 URL，请使用扩展方法 <see cref="EmoteExtensions.GetDynamicResourceUrl" />。
     /// </remarks>
     public string? DynamicImage { get; internal set; }
 
