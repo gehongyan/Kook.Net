73193bdebf2cf7fbcbada0984e23f0d06aa6fbb4
Gehongyan <gehongyan1996@126.com>
2026-02-11 15:40:03 +0800

    Remove redundant property definitions in QueueItem struct

--- src/Kook.Net.MessageQueue.InMemory/Data/QueueItem.cs
+++ src/Kook.Net.MessageQueue.InMemory/Data/QueueItem.cs
@@ -2,8 +2,4 @@
 
 namespace Kook.Net.Queue.InMemory;
 
-internal readonly record struct QueueItem(int Sequence, JsonElement Payload)
-{
-    public int Sequence { get; } = Sequence;
-    public JsonElement Payload { get; } = Payload;
-}
+internal readonly record struct QueueItem(int Sequence, JsonElement Payload);
