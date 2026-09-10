1ad84b6fe23081883e04730bb884e862a8e80b0d
Gehongyan <gehongyan1996@126.com>
2026-09-10 16:00:05 +0800

    优化 XML 文档

--- src/Kook.Net.Core/Entities/Guilds/GuildSearchSetting.cs
+++ src/Kook.Net.Core/Entities/Guilds/GuildSearchSetting.cs
@@ -6,17 +6,17 @@
 public enum GuildSearchSetting
 {
     /// <summary>
-    ///     不公开，不允许通过ID或关键词搜索加入
+    ///     不公开，不允许通过 ID 或关键词搜索加入。
     /// </summary>
     Private = 1,
 
     /// <summary>
-    ///     仅允许通过 ID 搜索与加入
+    ///     仅允许通过 ID 搜索与加入。
     /// </summary>
     IdOnly = 2,
 
     /// <summary>
-    ///     允许通过 ID 或关键词搜索与加入
+    ///     允许通过 ID 或关键词搜索与加入。
     /// </summary>
     Public = 3
 }
